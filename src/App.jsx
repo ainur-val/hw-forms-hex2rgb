c22f7c7ddcf98c60b59507d2622761af5e36a30f
Aynur Valiev <valiev.ainur.1991@gmail.com>
2025-08-25 23:41:32 +0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 let resultRgb;
+let textRgb
 
 class App extends React.Component {
      constructor(props) {
@@ -22,31 +23,33 @@ class App extends React.Component {
       let r = parseInt(event.target.value.slice(1, 3), 16);  
       let g = parseInt(event.target.value.slice(3, 5), 16);  
       let b = parseInt(event.target.value.slice(5, 7), 16);  
-      resultRgb = `rgb(${r}, ${g}, ${b})`
-      document.body.style.backgroundColor = resultRgb;
-      this.setState({bodyColor: resultRgb});
+      textRgb = `rgb(${r}, ${g}, ${b})`
+      resultRgb = textRgb;
+      this.setState({bodyColor: textRgb});
       
       } else {
-        resultRgb = "Ошибка!";
-        document.body.style.backgroundColor = "white";
+        textRgb = "Ошибка!";
+        resultRgb = "white";
       }
     } else if (event.target.value.length > 7) {
-        resultRgb = "Ошибка!";
-        document.body.style.backgroundColor = "white";
+        textRgb = "Ошибка!";
+        resultRgb = "white";
       } else if (event.target.value.length < 7) {
-        resultRgb = "";
-        document.body.style.backgroundColor = "white";
+        textRgb = "";
+        resultRgb = "white";
       }
   }
   
   render() {
     return (
+      <div className='container' style={{ background: resultRgb }}>
       <form>
         <label>
           <input className="divInput" type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
-        <div className="divColor">{resultRgb}</div>
+        <div className="divColor">{textRgb}</div>
         </form>
+        </div>
     );
   }
 }
